b1872628d83bb4f14b02784e81af6a225a58eacf
airivinxx <84199346+Elizabethafton-bit@users.noreply.github.com>
2026-07-09 14:25:28 -0400

    Reduce radius of circle markers in travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -9,6 +9,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker(coordinates).bindPopup("<p>Hello, from Kyoto City!</p>").addTo(map);
-var marker1 = L.circleMarker(place1, {color: "black", fillColor: "#a37936",radius: 5000}).bindPopup("<p>Hello, from Nara city!</p>").addTo(map);
-var marker2 = L.circleMarker(place2, {color: "black", fillColor: "#33419A",radius: 5000}).bindPopup("<p>Hello, from Osaka city!</p>").addTo(map);
-var marker3 = L.circleMarker(place3, {color: "black", fillColor: "#50C878",radius: 5000}).bindPopup("<p>Hello, from Uji city!</p>").addTo(map);
+var marker1 = L.circleMarker(place1, {color: "black", fillColor: "#a37936",radius: 100}).bindPopup("<p>Hello, from Nara city!</p>").addTo(map);
+var marker2 = L.circleMarker(place2, {color: "black", fillColor: "#33419A",radius: 100}).bindPopup("<p>Hello, from Osaka city!</p>").addTo(map);
+var marker3 = L.circleMarker(place3, {color: "black", fillColor: "#50C878",radius: 100}).bindPopup("<p>Hello, from Uji city!</p>").addTo(map);
